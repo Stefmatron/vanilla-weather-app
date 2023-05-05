bbbda1c3dad1370fde4b6adaeb48faefd54814da
Stefanie <131598782+Stefmatron@users.noreply.github.com>
2023-05-05 21:03:19 +0200

    added js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 let apiKey = "6af364afb0c9b134to765939b0fbbf44";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`
+let apiUrl = `https://api.shecodes.io/weather/v1/current?lisbon&key=${apiKey}`;
 
+function currentTemp(response) {
+    console.log(response);
+}
+
+function initData() {
+    axios.get(apiUrl).then(currentTemp);
+}
