beeb4a725db970ce6e5a1523848281a8962efc6f
Stefanie <131598782+Stefmatron@users.noreply.github.com>
2023-05-10 21:17:07 +0200

    added forecast with live data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-function currentTemp(response) {
+function getCurrentTemp(response) {
     let TempElement = document.querySelector("#cityDeg");
     let cityElement = document.querySelector("#cityName");
     let windElement = document.querySelector("#cityWin");
     let desElement = document.querySelector("#cityDes");
     let citIcon = document.querySelector("#cityIcon");
     let humElement = document.querySelector("#cityHum")
-   console.log('#', response, humElement);
+    console.log('#', response, humElement);
     TempElement.innerHTML = Math.round (response.data.temperature.current);
     humElement.innerHTML = response.data.temperature.humidity;
     console.log('#', response.data.temperature.current);
@@ -16,12 +16,25 @@ function currentTemp(response) {
     desElement.innerHTML = response.data.condition.description;
     windElement.innerHTML = Math.round (response.data.wind.speed);
     citIcon.setAttribute("src", response.data.condition.icon_url);
+
+    console.log(response.data);
+    console.log('#repsonse', response, response.data, celsiusTemp);
+    getForecast(response.data.coordinates);
+
 }
+
+function getForecast(coordinates) {
+    console.log('#coord', coordinates);
+     let apiKey = "6af364afb0c9b134to765939b0fbbf44";
+     let apiUrl =`https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+     console.log(apiUrl);
+     axios.get(apiUrl).then(currentTemp);
+    }
 
 function search(city) {
     console.log(city);
     let apiUrl = getApiData(city);
-    axios.get(apiUrl).then(currentTemp);
+    axios.get(apiUrl).then(getCurrentTemp);
     let h1 = document.querySelector("#cityName");
     h1.innerHTML = city; return false;
 }
@@ -45,7 +58,7 @@ function showCelsius(event) {
 function initData() {
     let city = "Argenthal";
     let apiUrl = getApiData(city);
-    axios.get(apiUrl).then(currentTemp);
+    axios.get(apiUrl).then(getCurrentTemp);
 }
 
 function getApiData(city) {
@@ -61,22 +74,32 @@ function enter(event) {
     }
 }
 
-function currentTemp() {
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu"];
+
+    return days[day];
+}
+
+function currentTemp(response) {
+    console.log('#daily', response.data);
+    let currentForecast = response.data?.daily;
+    
     let forecastElement = document.querySelector("#forecastDays");
 
-    let day = ["Thu", "Fri", "Sat", "Sun"];
-
     let forecastHTML = `<div class="row">`;
-    day.forEach(function (day) {
+    currentForecast.forEach(function (forecastDay, index) {
+    
         forecastHTML =
             forecastHTML +
             `
             <div class="col-2">
-              <div class="forecastDays">${day}</div>
-            <img src="../src/icons/rain-day.png" alt="rain-day" class="img-icon"/>
+              <div class="forecastDays">${formatDay(forecastDay.time)}</div>
+            <img src="${forecastDay.condition.icon_url}" alt="rain-day" class="img-icon"/>
             <div class="forecast-temp">
-              <span class="forecast-temp-max"> 18° </span>
-              <span class="forecast-temp-min"> 12° </span>
+              <span class="forecast-temp-max"> ${Math.round(forecastDay.temperature.maximum)}° </span>
+              <span class="forecast-temp-min"> ${Math.round(forecastDay.temperature.minimum)}° </span>
             </div>
             </div>
             `;
@@ -85,6 +108,8 @@ forecastHTML = forecastHTML + `</div>`;
 forecastElement.innerHTML = forecastHTML;
 console.log(forecastHTML);
     }
+
+
 
 
 initData();
